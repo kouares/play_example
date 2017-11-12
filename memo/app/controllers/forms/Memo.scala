4afcb1d82f633a462924a0abe1803ff684c4a18a
Unknown <ares.fanovaru@gmail.com>
2017-11-13 00:07:46 +0900

    DB設計に合わせる

--- memo/app/controllers/forms/Memo.scala
+++ memo/app/controllers/forms/Memo.scala
@@ -4,12 +4,12 @@ import play.api.data._
 import play.api.data.Forms._
 
 object Memo {
-  case class MemoForm(id: Option[Int], title: String, mainText: String, tags: Seq[String])
+  case class MemoForm(id: Option[Int], title: String, mainText: Option[String], tags: Seq[String])
 
   val memoForm = Form(
     mapping(
       "id" -> optional(number),
       "title" -> nonEmptyText(maxLength = 200),
-      "mainText" -> text,
+      "mainText" -> optional(text),
       "tags" -> seq(text))(MemoForm.apply)(MemoForm.unapply))
 }
