f05bebcf512bfcaf0da6888750cb94343ea25582
Unknown <ares.fanovaru@gmail.com>
2017-11-13 02:07:43 +0900

    WIP

--- memo/app/dao/MemoDao.scala
+++ memo/app/dao/MemoDao.scala
@@ -18,20 +18,7 @@ import play.api.Logger
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.db.slick.HasDatabaseConfigProvider
 import slick.jdbc.JdbcProfile
-import slick.jdbc.MySQLProfile.api.DBIO
-import slick.jdbc.MySQLProfile.api.anyOptionExtensionMethods
-import slick.jdbc.MySQLProfile.api.booleanColumnType
-import slick.jdbc.MySQLProfile.api.columnExtensionMethods
-import slick.jdbc.MySQLProfile.api.columnToOrdered
-import slick.jdbc.MySQLProfile.api.intColumnType
-import slick.jdbc.MySQLProfile.api.jdbcActionExtensionMethods
-import slick.jdbc.MySQLProfile.api.optionColumnExtensionMethods
-import slick.jdbc.MySQLProfile.api.queryInsertActionExtensionMethods
-import slick.jdbc.MySQLProfile.api.recordQueryActionExtensionMethods
-import slick.jdbc.MySQLProfile.api.streamableQueryActionExtensionMethods
-import slick.jdbc.MySQLProfile.api.stringColumnType
-import slick.jdbc.MySQLProfile.api.stringOptionColumnExtensionMethods
-import slick.jdbc.MySQLProfile.api.valueToConstColumn
+import slick.jdbc.MySQLProfile.api._
 
 case class MemoInfo(id: Int, title: String, mainText: String, tags: Option[Seq[TagMstRow]])
 
@@ -42,6 +29,8 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
   def search(mainText: Option[String]): Future[Seq[MemoInfo]]
 
   def create(form: MemoForm): Future[Int]
+
+  def update(form: MemoForm): Future[Int]
 }
 
 @Singleton class MemoDaoImpl @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) extends MemoDao {
@@ -70,12 +59,45 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
       tagIds <- DBIO.sequence(form.tags.map(tag =>
         TagMst.filter(_.name === tag).exists.result.flatMap {
           case true => DBIO.successful(0)
-          case falde => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
+          case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
         }))
       _ <- TagMst.filter(_.name.inSetBind(form.tags)).result.flatMap(tags =>
         TagMapping ++= tags.map(tag => TagMappingRow(Some(memoId), tag.id)))
     } yield (memoId)).transactionally
 
     db.run(actions)
+  }
+
+  def update(form: MemoForm) = {
+    val actions = (for {
+      memoId <- Memo.filter(memo => memo.id === form.id.getOrElse(-1).bind)
+        .map(result => (result.title, result.mainText, result.upadtedAt))
+        .update((form.title, form.mainText, Some(new Timestamp(System.currentTimeMillis()))))
+      tagIds <- DBIO.sequence(form.tags.filter(!_.endsWith("-remove")).map(tag =>
+        TagMst.filter(_.name === tag).exists.result.flatMap {
+          case true => DBIO.successful(0)
+          case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
+        }))
+      _ <- form.tags.map(tag =>
+        tag.endsWith("-remove") match {
+          case true => TagMst.filter(_.name === tag.replace("-remove", "")).result.headOption.flatMap(_.fold(
+            DBIO.successful_v))(tagMst =>
+              TagMapping.filter(tagMap => tagMap.memoId === form.id && tagMap.tagId === tagMst.id).delete.map(id => id)))
+          case false => TagMst.filter(_.name === tag).result.headOption.flatMap(_.fold(
+            DBIO.successful(0))(tagMst =>
+              TagMapping.filter(tagMap => tagMap.memoId === form.id && tagMap.tagId === tagMst.id).exists.result.flatMap {
+                case true => DBIO.successful(false)
+                case false => TagMapping += TagMappingRow(Some(memoId), tagMst.id)
+              }))
+        })
+    } yield (memoId)).transactionally
+
+    db.run(actions)
+    //      .map(result => (result.title, result.mainText, result.upadtedAt))
+    //      .update((form.title, form.mainText, new Timestamp(System.currentTimeMillis())))
+    //    val actions = (for {
+    //      Memo.filter(m)
+    //    } yield ()).transactionally
+    Future { 0 }
   }
 }
