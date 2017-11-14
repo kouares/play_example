d97c3c840b8ac13e4378ffc18a3474fa4238c98b
kouares <ares.fanovaru@gmail.com>
2017-11-14 16:08:44 +0900

    コメント追加とリファクタ

--- memo/app/dao/MemoDao.scala
+++ memo/app/dao/MemoDao.scala
@@ -30,7 +30,7 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
 
   def create(form: MemoForm): Future[Int]
 
-  def update(form: MemoForm): Future[Int]
+  def update(form: MemoForm): Future[Option[Int]]
 
   def delete(id: Int): Future[Int]
 }
@@ -38,12 +38,13 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
 @Singleton class MemoDaoImpl @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) extends MemoDao {
 
   def search(mainText: Option[String]) = {
-    val memoQuery = mainText.fold(Memo.sortBy(_.id).map(result => (result.id, result.title, result.mainText.getOrElse(""))))(mainText =>
-      Memo.filter(_.mainText like s"%${mainText}%").map(result => (result.id, result.title, result.mainText.getOrElse(""))))
-      .result
-
     for {
-      memos <- db.run(memoQuery)
+      // 検索条件が存在していれば条件を使って検索、存在していないなら全件取得
+      memos <- db.run(mainText.fold(Memo.sortBy(_.id).map(result => (result.id, result.title, result.mainText.getOrElse(""))))(mainText =>
+        Memo.filter(_.mainText like s"%${mainText}%").map(result => (result.id, result.title, result.mainText.getOrElse(""))))
+        .result)
+
+      // 取得したメモに紐づくタグを取得する
       tags <- {
         db.run(Memo.filter(_.id.inSetBind(memos.map(_._1)))
           .join(TagMapping).on((m, t) => m.id === t.memoId)
@@ -52,17 +53,26 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
           .result)
           .map(result => if (result.flatten.isEmpty) None else Some(result.flatten))
       }
-    } yield memos.map(memo => MemoInfo(memo._1, memo._2, memo._3, tags))
+    } yield {
+      // 取得したメモと紐づくタグのリストを持つ型に変換
+      memos.map(memo => MemoInfo(memo._1, memo._2, memo._3, tags))
+    }
   }
 
   def create(form: MemoForm) = {
+    // トランザクションを作成
     val actions = (for {
+      // メモを登録してidを取得
       memoId <- Memo returning Memo.map(_.id) += MemoRow(form.title, form.mainText, None, new Timestamp(System.currentTimeMillis()))
-      tagIds <- DBIO.sequence(form.tags.map(tag =>
+
+      // ひも付けられたタグがマスタに存在しない場合、タグを登録
+      _ <- DBIO.sequence(form.tags.map(tag =>
         TagMst.filter(_.name === tag).exists.result.flatMap {
           case true => DBIO.successful(0)
           case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
         }))
+
+      // メモとタグのマッピングを登録
       _ <- TagMst.filter(_.name.inSetBind(form.tags)).result.flatMap(tags =>
         TagMapping ++= tags.map(tag => TagMappingRow(Some(memoId), tag.id)))
     } yield (memoId)).transactionally
@@ -71,31 +81,41 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
   }
 
   def update(form: MemoForm) = {
+    // トランザクションを作成
     val actions = (for {
-      memoId <- Memo.filter(memo => memo.id === form.id.getOrElse(-1).bind)
+      // メモをアップデートして更新したメモのidを取得
+      updatedMemoId <- Memo.filter(memo => memo.id === form.id.getOrElse(-1).bind)
         .map(result => (result.title, result.mainText, result.upadtedAt))
-        .update((form.title, form.mainText, Some(new Timestamp(System.currentTimeMillis()))))
-      tagIds <- DBIO.sequence(form.tags.filter(!_.endsWith("-remove")).map(tag =>
+        .update((form.title, form.mainText, Some(new Timestamp(System.currentTimeMillis())))).map(_ match {
+          case updated if updated == 1 => form.id
+          case _ => None
+        })
+
+      // タグのひも付き解除でないタグでマスタに存在しない場合、タグを登録
+      _ <- DBIO.sequence(form.tags.filter(!_.endsWith("-remove")).map(tag =>
         TagMst.filter(_.name === tag).exists.result.flatMap {
           case true => DBIO.successful(0)
           case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
         }))
+
+      // タグのひも付きの削除と登録
       _ <- DBIO.sequence(form.tags.map(tag =>
         tag.endsWith("-remove") match {
           case true => TagMst.filter(_.name === tag.replace("-remove", "")).result.headOption.map(_.map(tagMstRow =>
-            TagMapping.filter(tagMapping => tagMapping.memoId === form.id && tagMapping.tagId === tagMstRow.id).delete))
+            TagMapping.filter(tagMapping => tagMapping.memoId === updatedMemoId && tagMapping.tagId === tagMstRow.id).delete))
           case false => TagMst.filter(_.name === tag).result.headOption.map(_.map(tagMstRow =>
-            TagMapping.filter(tagMapping => tagMapping.memoId === form.id && tagMapping.tagId === tagMstRow.id).exists.result.map {
+            TagMapping.filter(tagMapping => tagMapping.memoId === updatedMemoId && tagMapping.tagId === tagMstRow.id).exists.result.map {
               case true => DBIO.successful(false)
-              case false => TagMapping += TagMappingRow(Some(memoId), tagMstRow.id)
+              case false => TagMapping += TagMappingRow(updatedMemoId, tagMstRow.id)
             }))
         }))
-    } yield (memoId)).transactionally
+    } yield (updatedMemoId)).transactionally
 
     db.run(actions)
   }
 
   def delete(id: Int) = {
+    // メモとタグのひも付きを削除
     db.run(Memo.filter(_.id === id).delete.flatMap(_ => TagMapping.filter(_.memoId === id).delete))
   }
 }
