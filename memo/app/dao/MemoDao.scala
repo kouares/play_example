057ed7f385b9c15ae8c55de8bdcc47504b029675
kouares <ares.fanovaru@gmail.com>
2017-11-14 16:55:33 +0900

    リファクタその２

--- memo/app/dao/MemoDao.scala
+++ memo/app/dao/MemoDao.scala
@@ -40,19 +40,18 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
   def search(mainText: Option[String]) = {
     for {
       // 検索条件が存在していれば条件を使って検索、存在していないなら全件取得
-      memos <- db.run(mainText.fold(Memo.sortBy(_.id).map(result => (result.id, result.title, result.mainText.getOrElse(""))))(mainText =>
-        Memo.filter(_.mainText like s"%${mainText}%").map(result => (result.id, result.title, result.mainText.getOrElse(""))))
-        .result)
+      memos <- db.run(mainText.fold(
+        Memo.sortBy(_.id).map(result =>
+          (result.id, result.title, result.mainText.getOrElse(""))))(mainText =>
+          Memo.filter(_.mainText like s"%${mainText}%").map(result =>
+            (result.id, result.title, result.mainText.getOrElse("")))).result)
 
       // 取得したメモに紐づくタグを取得する
-      tags <- {
-        db.run(Memo.filter(_.id.inSetBind(memos.map(_._1)))
-          .join(TagMapping).on((m, t) => m.id === t.memoId)
-          .joinLeft(TagMst).on((mt, tm) => mt._2.tagId === tm.id)
-          .map(result => result._2)
-          .result)
-          .map(result => if (result.flatten.isEmpty) None else Some(result.flatten))
-      }
+      tags <- db.run(Memo.filter(_.id.inSetBind(memos.map(_._1)))
+        .join(TagMapping).on((m, t) => m.id === t.memoId)
+        .joinLeft(TagMst).on((mt, tm) => mt._2.tagId === tm.id)
+        .map(result => result._2).result)
+        .map(result => if (result.flatten.isEmpty) None else Some(result.flatten))
     } yield {
       // 取得したメモと紐づくタグのリストを持つ型に変換
       memos.map(memo => MemoInfo(memo._1, memo._2, memo._3, tags))
@@ -93,10 +92,11 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
 
       // タグのひも付き解除でないタグでマスタに存在しない場合、タグを登録
       _ <- DBIO.sequence(form.tags.filter(!_.endsWith("-remove")).map(tag =>
-        TagMst.filter(_.name === tag).exists.result.flatMap {
-          case true => DBIO.successful(0)
-          case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
-        }))
+        TagMst.filter(_.name === tag)
+          .exists.result.flatMap {
+            case true => DBIO.successful(0)
+            case false => (TagMst returning TagMst.map(_.id) += TagMstRow(Some(tag))).map(DBIO.successful(_))
+          }))
 
       // タグのひも付きの削除と登録
       _ <- DBIO.sequence(form.tags.map(tag =>
@@ -104,10 +104,11 @@ trait MemoDao extends HasDatabaseConfigProvider[JdbcProfile] {
           case true => TagMst.filter(_.name === tag.replace("-remove", "")).result.headOption.map(_.map(tagMstRow =>
             TagMapping.filter(tagMapping => tagMapping.memoId === updatedMemoId && tagMapping.tagId === tagMstRow.id).delete))
           case false => TagMst.filter(_.name === tag).result.headOption.map(_.map(tagMstRow =>
-            TagMapping.filter(tagMapping => tagMapping.memoId === updatedMemoId && tagMapping.tagId === tagMstRow.id).exists.result.map {
-              case true => DBIO.successful(false)
-              case false => TagMapping += TagMappingRow(updatedMemoId, tagMstRow.id)
-            }))
+            TagMapping.filter(tagMapping => tagMapping.memoId === updatedMemoId && tagMapping.tagId === tagMstRow.id).exists.result
+              .map {
+                case true => DBIO.successful(false)
+                case false => TagMapping += TagMappingRow(updatedMemoId, tagMstRow.id)
+              }))
         }))
     } yield (updatedMemoId)).transactionally
 
